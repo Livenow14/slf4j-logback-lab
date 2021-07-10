60dd00d01487d3c27d2fde3ecdf8f6cdaef35d96
TaeJeong Kim <xowjd41@naver.com>
2021-07-10 14:35:17 +0900

    feat: System.out.println()을 이용한 로깅

--- src/main/java/lab/LabApplication.java
+++ src/main/java/lab/LabApplication.java
@@ -2,6 +2,8 @@ package lab;
 
 public class LabApplication {
     public static void main(String[] args) {
-
+        for (int count = 1; count <= 100; count++) {
+            System.out.println("로깅 재밌어 " + count);
+        }
     }
 }
