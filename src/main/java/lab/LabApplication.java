c2d725cda2224d153e3e38273a6b428b1de264d8
TaeJeong Kim <xowjd41@naver.com>
2021-07-10 14:59:34 +0900

    feat: logback-classic, logback-core 라이브러리 추가
    logback-classic는 자신에게 의존하는 logback-core-1.2.3.jar뿐만 아니라 slf4j-api-1.7.25.jar를 자동으로 가져옴,
    
    해당 artifact의 올바른 버전을 사용하는데 필요하기 때문에 모두 명시적으로 선언하는 것이 좋기에 exclude에 추가

--- src/main/java/lab/LabApplication.java
+++ src/main/java/lab/LabApplication.java
@@ -5,13 +5,17 @@ import org.slf4j.LoggerFactory;
 
 public class LabApplication {
     public static void main(String[] args) {
-        for (int count = 1; count <= 100; count++) {
+        for (int count = 1; count <= 10; count++) {
             System.out.println("로깅 재밌어 " + count);
         }
 
         Logger logger = LoggerFactory.getLogger(LabApplication.class);
-        for (int count = 1; count <= 100; count++) {
+        for (int count = 1; count <= 10; count++) {
             logger.info("SLF4J-API를 사용하는 로깅 재밌어 {}", count);
+        }
+
+        for (int count = 1; count <= 10; count++) {
+            logger.info("SLF4J-API와 Logback 로깅 재밌어 {}", count);
         }
     }
 }
