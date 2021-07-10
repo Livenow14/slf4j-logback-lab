fad48def5ef80214b3000099abf7d60cac39e0f1
TaeJeong Kim <xowjd41@naver.com>
2021-07-10 16:41:16 +0900

    Springboot Initialize Setting

---
+++ src/main/java/com/livenow/slf4jlogbacklab/Slf4jLogbackLabApplication.java
@@ -0,0 +1,13 @@
+package com.livenow.slf4jlogbacklab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slf4jLogbackLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Slf4jLogbackLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/livenow/slf4jlogbacklab/Slf4jLogbackLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livenow.slf4jlogbacklab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slf4jLogbackLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
