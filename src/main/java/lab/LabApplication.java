f7bf577b4e542a7c277dee82f7f4d96149d4ce08
TaeJeong Kim <xowjd41@naver.com>
2021-07-10 14:43:28 +0900

    feat: slf4j-api를 이용한 로깅 (실패)
    바인딩 라이브러리가 추가되지 않아 실패함

--- src/main/java/lab/LabApplication.java
+++ src/main/java/lab/LabApplication.java
@@ -1,9 +1,17 @@
 package lab;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LabApplication {
     public static void main(String[] args) {
         for (int count = 1; count <= 100; count++) {
             System.out.println("로깅 재밌어 " + count);
+        }
+
+        Logger logger = LoggerFactory.getLogger(LabApplication.class);
+        for (int count = 1; count <= 100; count++) {
+            logger.info("SLF4J-API를 사용하는 로깅 재밌어 {}", count);
         }
     }
 }
